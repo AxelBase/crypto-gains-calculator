e8a2523e7eaa7c8461edebd28d061aa313e8a350
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-12-27 22:24:21 +0200

    GSC

---
+++ docs/_app/immutable/chunks/CRDnNZFn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ap4u8t?.base??"/crypto-gains-calculator",a=globalThis.__sveltekit_1ap4u8t?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/EBxT-cte.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_fqptfe?.base??"/crypto-gains-calculator",t=globalThis.__sveltekit_fqptfe?.assets??s??"";export{t as a,s as b};
